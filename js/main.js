098713b98029f48d081844c8babc2b49458a1aaa
vEnoch24 <144890721+vEnoch24@users.noreply.github.com>
2024-08-25 13:18:10 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+        layoutMode: 'fitRows',
     });
     $('#portfolio-flters li').on('click', function () {
         $("#portfolio-flters li").removeClass('active');
